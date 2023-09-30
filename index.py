867309aaaedb33f5f4773d1472a44bf661d4b4bd
Breath <119684204+BreathXV@users.noreply.github.com>
2023-09-30 22:30:19 +0100

    v2.1.1
    Cogs and Index

---
+++ index.py
@@ -0,0 +1,8 @@
+import discord
+from discord.ext import commands
+
+from cogs.ticket_system import TicketSystem 
+from cogs.management_system import ManagementPanel
+from cogs.statuses import Statuses
+from cogs.project_management import ProjectManagement
+from cogs.utilities import Utilites
