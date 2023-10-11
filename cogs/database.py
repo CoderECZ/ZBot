09b7549ddd27365fba1e528cbc1b947e5a89462d
Breath <breath@coderz.biz>
2023-10-11 16:23:24 +0100

    Added database.py cog

---
+++ cogs/database.py
@@ -0,0 +1,57 @@
+import mysql.connector as mysql
+import json
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+    db_config = {
+        'host': config['database']['host'],
+        'user': config['database']['user'],
+        'password': config['database']['password'],
+        'database': config['database']['database'],
+    }
+
+class Database:
+    def __init__(self, database, connection, cursor):
+        self.database = db_config
+        self.connection = mysql.connector.connect(**self.database)
+        self.cursor = self.connection.cursor()
+    
+    def query(self, query):
+        try:
+            self.cursor.execute(query)
+        except mysql.connector.Error as err:
+            return err
+        finally:
+            self.cursor.commit()
+            self.cursor.close()
+    
+    def fetch(self, query, fetchmany=None, fetchone:bool=True, fetchall:bool=False):
+        if fetchall is not False or fetchmany is not None:
+            if isinstance(fetchmany, int):
+                try:
+                    self.cursor.execute(query)
+                    return self.cursor.fetchmany(fetchmany)
+                except mysql.connector.Error as err:
+                    return err
+                finally:
+                    self.cursor.commit()
+                    self.cursor.close()
+            elif fetchall is True:
+                try:
+                    self.cursor.execute(query)
+                    return self.cursor.fetchall()
+                except mysql.connector.Error as err:
+                    return err
+                finally:
+                    self.cursor.commit()
+                    self.cursor.close()
+        else:
+            try:
+                self.cursor.execute(query)
+                return self.cursor.fetchone()
+            except mysql.connector.Error as err:
+                return err
+            finally:
+                self.cursor.commit()
+                self.cursor.close()
+        
