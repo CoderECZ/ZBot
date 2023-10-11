6633db16fd9f43ba34ee39f410a81ebf21fc3d69
Breath <breath@coderz.biz>
2023-10-11 16:02:47 +0100

    Added database initial query.

--- index.py
+++ index.py
@@ -26,6 +26,47 @@ with open('config.json', 'r') as f:
         'database': config['database']['database'],
     }
 
+try:  
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+    
+    initialQuery = """
+        START TRANSACTION;
+        CREATE TABLE IF NOT EXISTS developers (
+            user_id INT PRIMARY KEY,
+            developer_name VARCHAR(255),
+            developer_rank VARCHAR(255),
+            status VARCHAR(255)
+        );
+        CREATE TABLE IF NOT EXISTS certified_roles (
+            developer_id INT,
+            role_name VARCHAR(255),
+            FOREIGN KEY (developer_id) REFERENCES developers (user_id)
+        );
+        CREATE TABLE IF NOT EXISTS projects (
+            project_id VARCHAR(255) PRIMARY KEY,
+            game VARCHAR(255),
+            project_details TEXT,
+            developer_payment FLOAT,
+            deadline VARCHAR(255),
+            status VARCHAR(255),
+            assigned_to INT,
+            client INT
+        );
+        CREATE TABLE IF NOT EXISTS completed_projects (
+            developer_id INT PRIMARY KEY,
+            project_id VARCHAR(255),
+            developer_payment FLOAT
+        );
+        COMMIT;
+    """
+    
+    cursor.execute(initialQuery)
+except mysql.connector.Error as err:
+    print("Error: %s" % err)
+finally:
+    connection.commit()
+
 @bot.event
 async def on_ready():
     await bot.add_cog(TicketSystem(bot))
